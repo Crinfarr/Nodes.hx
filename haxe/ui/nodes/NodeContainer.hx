b0f256679309d2b04db8f700ce0b8a4458caa41c
Asher Hoeveler <theargon0@gmail.com>
2023-05-18 16:11:04 -0500

    add fun fake zoomy background

--- haxe/ui/nodes/NodeContainer.hx
+++ haxe/ui/nodes/NodeContainer.hx
@@ -1,9 +1,31 @@
 package haxe.ui.nodes;
 
+import haxe.ui.events.UIEvent;
+import haxe.ui.validation.InvalidationFlags;
+import haxe.ui.components.Image;
 import haxe.ui.components.Canvas;
+import haxe.ui.events.MouseEvent;
 
 class NodeContainer extends Canvas {
+	public var nodeScale:Float = 1;
+
 	public function new() {
 		super();
+		this.backgroundImage = "assets/bg.png";
+		this.customStyle.backgroundImageRepeat = "repeat";
+	}
+	
+	@:bind(this, MouseEvent.MOUSE_WHEEL)
+	private function mWheelZoom(e:MouseEvent) {
+		this.nodeScale += (0.01*e.delta);
+		this.customStyle.backgroundWidth = nodeScale*512;
+		this.customStyle.backgroundHeight = nodeScale*512;
+		this.invalidateComponent(InvalidationFlags.STYLE);
+		//TODO make this scale child nodes too
+	}
+
+	@:bind(this, UIEvent.COMPONENT_ADDED)
+	private function nodeAdded(e:UIEvent) {
+		trace(e);
 	}
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+# More documentation will be here when I get to it
+
+
+## Licenses
+Background image by Mintor94 from "[High quality portal 2 wall textures](https://gamebanana.com/mods/383184)"
+[![Creative Commons badge](https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-nd/4.0/)
+Licensed under [CC BY-NC-ND 4.0](https://creativecommons.org/licenses/by-nc-nd/4.0/)
