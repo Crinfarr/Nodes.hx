390b4c9e76a95235aa0de5e6a7a87c303950c8cd
Asher Hoeveler <theargon0@gmail.com>
2023-08-22 16:52:17 -0500

    Start working on node scaling

--- haxe/ui/nodes/Node.hx
+++ haxe/ui/nodes/Node.hx
@@ -3,36 +3,47 @@ package haxe.ui.nodes;
 import haxe.ui.components.Label;
 import haxe.ui.containers.VBox;
 import haxe.ui.components.Canvas;
-
 import haxe.ui.nodes._internal.NodeProperty;
 import haxe.ui.nodes._internal.enums.NodeInputType;
 
 class Node extends VBox {
-    public var properties:Array<NodeProperty>;
-    public var name:Label;
-
-    public function new(n:String) {
-        super();
-
-        this.name = new Label();
-        this.name.textAlign = "center";
-        this.name.percentWidth = 100;
-        this.name.autoHeight = true;
-        this.name.text = n;
-        this.addComponent(this.name);
-
-        this.properties = [];
-        this.styleString = "background-color: #6f6f6f";
-        this.autoWidth = true;
-        this.autoHeight = true;
+	public var properties:Array<NodeProperty>;
+	public var name:Label;
+	public var scale(get, set):Float = 1;
+    function get_scale():Float {
+        return scale;
     }
-    
-    public function addProperty(name:String, type:NodeInputType) {
-        var prop = new NodeProperty();
-        prop.setDataType(type);
-        prop.label.text = name;
-
-        this.properties.push(prop);
-        this.addComponent(prop);
+    function set_scale(v:Float) {
+        scale = v;
+		for (c in this.childComponents) {
+			c.width *= scale;
+            c.height *= scale;
+		};
     }
+
+	public function new(n:String) {
+		super();
+
+		this.name = new Label();
+		this.name.textAlign = "center";
+		this.name.percentWidth = 100;
+		this.name.autoHeight = true;
+		this.name.text = n;
+		this.addComponent(this.name);
+
+		this.properties = [];
+		this.styleString = "background-color: #6f6f6f";
+		this.autoWidth = true;
+		this.autoHeight = true;
+	}
+
+	public function addProperty(name:String, type:NodeInputType) {
+		var prop = new NodeProperty();
+		prop.setDataType(type);
+		prop.label.text = name;
+
+		this.properties.push(prop);
+		this.addComponent(prop);
+		
+	}
 }
--- haxe/ui/nodes/NodeContainer.hx
+++ haxe/ui/nodes/NodeContainer.hx
@@ -1,5 +1,6 @@
 package haxe.ui.nodes;
 
+import haxe.ui.core.Component;
 import haxe.ui.events.UIEvent;
 import haxe.ui.validation.InvalidationFlags;
 import haxe.ui.components.Image;
@@ -7,21 +8,35 @@ import haxe.ui.components.Canvas;
 import haxe.ui.events.MouseEvent;
 
 class NodeContainer extends Canvas {
-	public var nodeScale:Float = 1;
+	public var scale:Float = 1;
+
+	private var nodes:Array<Node> = [];
 
 	public function new() {
 		super();
 		this.backgroundImage = "assets/bg.png";
 		this.customStyle.backgroundImageRepeat = "repeat";
 	}
-	
+
 	@:bind(this, MouseEvent.MOUSE_WHEEL)
 	private function mWheelZoom(e:MouseEvent) {
-		this.nodeScale += (0.01*e.delta);
-		this.customStyle.backgroundWidth = nodeScale*512;
-		this.customStyle.backgroundHeight = nodeScale*512;
-		this.invalidateComponent(InvalidationFlags.STYLE);
-		//TODO make this scale child nodes too
+		this.nodeScale += (0.01 * e.delta);
+		this.customStyle.backgroundWidth = nodeScale * 512;
+		this.customStyle.backgroundHeight = nodeScale * 512;
+		this.invalidateComponent(STYLE);
+		// TODO make this scale child nodes too
+	}
+
+	override public function addComponent(c:Component) {
+		if (Type.getClass(c) == node) {
+			nodes.push(c);
+		}
+		super.addComponent(c);
+	}
+
+	override public function removeComponent(c:Component, dispose:Bool = true, invalidate:Bool = true) {
+		nodes.remove(c);
+		super.removeComponent(c, dispose, invalidate);
 	}
 
 	@:bind(this, UIEvent.COMPONENT_ADDED)
