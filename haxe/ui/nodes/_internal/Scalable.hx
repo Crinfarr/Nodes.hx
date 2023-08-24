cf980dbe67ce17bad31120cbc6679507cd0e1ad3
Asher Hoeveler <theargon0@gmail.com>
2023-08-24 16:39:32 -0500

    Convert to interface

--- haxe/ui/nodes/_internal/Scalable.hx
+++ haxe/ui/nodes/_internal/Scalable.hx
@@ -1,5 +1,6 @@
 package haxe.ui.nodes._internal;
 
-@:genericBuild(haxe.ui.nodes._internal.Macros.resolveScalable())
-@:autoBuild(haxe.ui.nodes._internal.Macros.addScalableFields())
-class Scalable<T> {}
+@:autoBuild(haxe.ui.nodes._internal.Macros.applyScalable())
+interface Scalable {
+    
+}
