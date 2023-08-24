c7187a4acf0f2d4141cf3fe084f9d1b8e8d57801
Asher Hoeveler <theargon0@gmail.com>
2023-08-24 16:39:40 -0500

    Implement basic get/set

--- haxe/ui/nodes/_internal/Macros.hx
+++ haxe/ui/nodes/_internal/Macros.hx
@@ -1,33 +1,67 @@
 package haxe.ui.nodes._internal;
 
+import haxe.macro.Expr.Field;
+import haxe.rtti.Meta;
+import haxe.macro.Compiler;
+#if macro
 import haxe.macro.Expr.FieldType;
 import haxe.macro.Expr.Access;
 import haxe.macro.Expr.ComplexType;
 import haxe.macro.Context;
 import haxe.macro.Type;
+#end
 
 class Macros {
-    public static macro function resolveScalable():ComplexType {
-        switch (Context.getLocalType()) {
-            case TInst(_, [t1]):
-                var type = Context.toComplexType(t1);
-                return Context.toComplexType(t1);
-            case t:
-                Context.fatalError('Scalable<T> must extend a class', Context.currentPos());
-                return null;
-        }
-    }
-    public static macro function addScalableFields():Array<Field> {
-        var fields = Context.getBuildFields();
-        var scalebase = 1;
-        var pos = Context.currentPos();
-        var fieldName = 'scale';
+	macro function applyScalable():Array<Field> {
+		var fields:Array<Field> = Context.getBuildFields();
+		
+		Compiler.addMetadata('@scalable', Context.getLocalClass().toString());
+		trace(Meta.getFields(Context.getLocalClass().get()));
 
-        var prop:Field = {
-            name: 'scale',
-            access: [Access.APublic],
-            kind: FieldType.FProp('get', 'set', macro:Float)
-        }
-        return Context.getBuildFields();
-    }
+		fields.push({
+			name: 'scale',
+			access: [APublic],
+			pos: Context.currentPos(),
+			kind: FProp('get', 'set', (macro :Float)),
+			meta: [
+				{
+					name: ':isVar',
+					pos: Context.currentPos()
+				}
+			]
+		});
+
+		fields.push({
+			name: 'get_scale',
+			kind: FFun({
+				args: [],
+				ret: (macro :Float),
+				expr: macro {
+					if (scale == null)
+						scale = 1;
+					return scale;
+				}
+			}),
+			pos: Context.currentPos()
+		});
+		fields.push({
+			name: 'set_scale',
+			kind: FFun({
+				args: [
+					{
+						name: 'value',
+						type: (macro :Float),
+					}
+				],
+				ret: (macro :Float),
+				expr: macro {
+					scale = value;
+					return scale;
+				}
+			}),
+			pos: Context.currentPos(),
+		});
+
+		return fields;
+	}
 }
