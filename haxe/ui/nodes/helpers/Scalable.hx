a01fe33cc16c364ab4dd7e9a32f3429c0022fa21
Asher Hoeveler <theargon0@gmail.com>
2023-08-30 17:58:21 -0500

    Add missing properties

--- haxe/ui/nodes/helpers/Scalable.hx
+++ haxe/ui/nodes/helpers/Scalable.hx
@@ -1,9 +1,14 @@
 package haxe.ui.nodes.helpers;
 
+#if !macro
+import haxe.ui.core.Component;
+#end
 @:autoBuild(haxe.ui.nodes._internal.Macros.applyScalable())
 interface Scalable {
 	private var baseWidth:Null<Float>;
 	private var baseHeight:Null<Float>;
+	private var scalableChildren:Array<Scalable>;
+	public function addComponent(component:Component):Component;
 	@:isVar public var scale(get, set):Float;
 	private function get_scale():Float;
 	private function set_scale(value:Float):Float;
